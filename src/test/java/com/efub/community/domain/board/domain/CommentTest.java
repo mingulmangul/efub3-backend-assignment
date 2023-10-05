4d0d6a4b99510bdac228d75f5df912410e9cee81
Kwon Min A <min2010011@gmail.com>
2023-10-06 02:16:28 +0900

    test: Comment 엔티티 테스트 코드 추가

---
+++ src/test/java/com/efub/community/domain/board/domain/CommentTest.java
@@ -0,0 +1,41 @@
+package com.efub.community.domain.board.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CommentTest extends BoardDomainEntityTest {
+
+	@Test
+	@DisplayName("댓글을 업데이트합니다.")
+	void update() {
+		// given
+		String newContent = "새로 업데이트되는 댓글의 내용입니다.";
+		// when
+		assertThat(comment.getContent()).isEqualTo(commentContent);
+		comment.updateComment(newContent);
+		// then
+		assertThat(comment.getContent()).isEqualTo(newContent);
+	}
+
+	@Test
+	@DisplayName("댓글 생성 시 댓글의 내용이 5자 미만이면, 예외가 발생합니다.")
+	void createShortPost() {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> Comment.builder()
+				.content("댓글")
+				.writer(member1)
+				.post(post1)
+				.anonymous(true)
+				.build());
+	}
+
+	@Test
+	@DisplayName("댓글에 게시글을 설정할 때 게시글이 null이 아니면, 예외가 발생합니다.")
+	void updateShortPost() {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> comment.setPost(post2));
+	}
+
+}
