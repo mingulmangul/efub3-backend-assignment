1bd5265cdcb92340cb63eede8f0db53fb74de9d1
Kwon Min A <min2010011@gmail.com>
2023-10-07 02:55:34 +0900

    remove: 불필요한 find 메소드 제거

--- src/main/java/com/efub/community/domain/board/repository/BoardRepository.java
+++ src/main/java/com/efub/community/domain/board/repository/BoardRepository.java
@@ -1,15 +1,12 @@
 package com.efub.community.domain.board.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.efub.community.domain.board.domain.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-	
-	Optional<Board> findById(Long boardId);
 
 	List<Board> findAllByOrderByBoardIdDesc();
 
